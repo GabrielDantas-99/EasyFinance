f5dfb4bd84738d88cfcb3305056eeb68acc854f1
Gabriel Dantas <gabriel.dantas@quark.tec.br>
2023-03-19 20:32:51 -0300

    [frontend] - Configuração inicial

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulo para requisições HTTP
+import { HttpClientModule } from '@angular/common/http';
+
+// Modulo para trabalhar com formulários
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import 'boxicons';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +18,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     // Requisições http
+     HttpClientModule,
+     // Forms
+     FormsModule,
+     ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
