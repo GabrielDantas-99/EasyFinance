5de5a1c0666b7213ea1f8520a0c5ff9f66d2219f
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-03-20 19:59:39 -0300

    [Frontend] Finalizando o componente overview

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,33 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
 // Modulo para requisições HTTP
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
 // Modulo para trabalhar com formulários
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import 'boxicons';
+import "boxicons";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { OverviewComponent } from './components/overview/overview.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { OverviewComponent } from "./components/overview/overview.component";
+import { MaterialModule } from "./shared/material/material.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OverviewComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-     // Requisições http
-     HttpClientModule,
-     // Forms
-     FormsModule,
-     ReactiveFormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, OverviewComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		// Requisições http
+		HttpClientModule,
+		// Forms
+		FormsModule,
+		ReactiveFormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/components/overview/overview.component.ts
+++ frontend/src/app/components/overview/overview.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
 
+  colunasLancamentos: string[] = ['Lançamento', 'Categoria', 'Valor', 'Forma de Pagamento', ''];
+
+  lancamentos: Lancamento[] = [
+    { id: 1, titulo: 'Playstation 5', categoria: 'Lazer', valor: 4999.97, formaPagamento: 'Crédito' },
+    { id: 1, titulo: 'Playstation 5', categoria: 'Lazer', valor: 4999.97, formaPagamento: 'Crédito' }
+  ];
+
+  ngOnInit(): void {
+  }
+
+  editar(id: number): void {
+    console.log("Editar: "+id);
+  }
+  
+  excluir(id: number): void {
+    console.log("Editar: "+id);
+  }
+
+}
+
+export interface Lancamento {
+  id?: number;
+  titulo: string;
+  categoria: string;
+  valor: number;
+  formaPagamento: string;
 }
